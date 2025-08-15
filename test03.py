dbd3f5876a0e4c690fdfbeab22976ca5d337c5b0
Taratorn09 <gramgramg4@gmail.com>
2025-08-15 11:44:55 +0700

    v.2

---
+++ test03.py
@@ -0,0 +1,16 @@
+#คำสั่งเเสดงช้อมูลใดๆ ทางหน้าจอ ใช้ฟังก็ฃั้น print()
+#strint
+print('hello')
+print('hi')
+print("hey")
+#numbar
+print(123456789)   #integer/int
+print(456.123)  #float
+
+#boolam ตรรกะ มิเเค่ True (จริง) เเละ False (เท็จ)
+print(True)
+print(False)
+
+#Expression นิพจน์ คือ อะไรก็ตามที่มี process เช่น การคำนวณ, ตัวแปร,
+print(20+50+90)
+print(8*20/2)
